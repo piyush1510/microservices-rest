469b908dac611313b67056e0d52ffa9efc8f6c71
piyush1510 <piyus15101999@gamil.com>
2021-10-24 20:56:10 +0530

    jwt with mongodb

---
+++ auth/jwt_mongodb/app.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const mongoose = require('mongoose');
+const express = require('express');
+const controller = require('./controller');
+const cookieParser = require('cookie-parser');
+
+mongoose.connect(
+    process.env.MONGO_URL
+    , {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName:process.env.DB_NAME
+    });
+
+const app = express();
+app
+    .use(express.json())
+    .use(cookieParser())
+    .post('/login',controller.login)
+    .post('/register',controller.register)
+    .get('/auth',controller.auth)
+    .use(controller.error404)
+    .use(controller.error500)
+    .listen(process.env.port, () => {
+        console.log(`Server started on ${process.env.port}`);
+});
